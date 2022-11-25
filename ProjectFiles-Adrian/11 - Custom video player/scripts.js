c8aaea2607a57912af4ff0ec979376f9af554ec3
AdrianSkar <adrian@adrianskar.com>
2022-11-25 22:52:35 +0100

    feat[adrian-11]: implement flag described at 14'

--- ProjectFiles-Adrian/11 - Custom video player/scripts.js
+++ ProjectFiles-Adrian/11 - Custom video player/scripts.js
@@ -10,22 +10,9 @@ const player = document.querySelector('.player'),
 
 // Build functions
 
-function togglePlay() {
-	const method = video.paused ? 'play' : 'pause';
-	video[method]();
-}
-
 function updateButtonStats() {
 	const icon = this.paused ? '▶' : '❚❚';
 	toggle.textContent = icon;
-}
-
-function skip() {
-	video.currentTime += parseFloat(this.dataset.skip);
-}
-
-function handleRangeUpdate() {
-	video[this.name] = this.value;
 }
 
 function handleProgress() {
@@ -33,11 +20,24 @@ function handleProgress() {
 	progressBar.style.flexBasis = `${percent}%`;
 }
 
+function togglePlay() {
+	const method = video.paused ? 'play' : 'pause';
+	video[method]();
+}
+
+function skip() {
+	video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+	if (!handleRangeMouseDown) return;
+	video[this.name] = this.value;
+}
+
 function scrub(e) {
 	const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
 	video.currentTime = scrubTime;
 }
-
 
 // Hook up listeners
 video.addEventListener('play', updateButtonStats);
@@ -45,11 +45,19 @@ video.addEventListener('pause', updateButtonStats);
 video.addEventListener('timeupdate', handleProgress);
 [video, toggle].forEach(el => el.addEventListener('click', togglePlay));
 skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+// Flag behavior as in Canvas' exercise (see return on function)
+let handleRangeMouseDown = false;
+ranges.forEach(range => {
+	range.addEventListener('change', handleRangeUpdate);
+	range.addEventListener('mousemove', handleRangeUpdate);
+	range.addEventListener('mousedown',() =>  (handleRangeMouseDown = true));
+	range.addEventListener('mouseup',() =>  (handleRangeMouseDown = false));
+});
+
+// Flag behavior w/o altering the function itself
 let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousemove', e => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => (mousedown = true));
 progress.addEventListener('mouseup', () => (mousedown = false));
