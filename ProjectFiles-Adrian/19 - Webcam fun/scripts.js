8c964dced276eaa43a63263eaba05246e61c2e50
AdrianSkar <adrian@adrianskar.com>
2023-02-03 14:26:54 +0100

    update[adrian-19]: up to 12', returning Interval?

--- ProjectFiles-Adrian/19 - Webcam fun/scripts.js
+++ ProjectFiles-Adrian/19 - Webcam fun/scripts.js
@@ -5,12 +5,15 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
+	// Get video feed from camera
 	navigator.mediaDevices
 		.getUserMedia({ video: true, audio: false })
 		.then(localMediaStream => {
-			console.log(localMediaStream);
+			// console.log(localMediaStream);
 			// Deprecated
 			// video.src = window.URL.createObjectURL(localMediaStream);
+
+			// set the feed as src and play it (otherwise it'll stay still)
 			video.srcObject = localMediaStream;
 			video.play();
 		})
@@ -19,4 +22,18 @@ function getVideo() {
 		});
 }
 
+function paintToCanvas() {
+	const [width, height] = [video.videoWidth, video.videoHeight];
+	canvas.width = width;
+	canvas.height = height;
+
+	// returning the Interval doesn't seem to mean that we can use clearInterval()
+	// as stated in 11'45". The only way that seems to work is by specifying the interval
+	// with its own ID outside the function so you can access and refer to it on the global scope
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 160);
+}
+
 getVideo();
+paintToCanvas();
