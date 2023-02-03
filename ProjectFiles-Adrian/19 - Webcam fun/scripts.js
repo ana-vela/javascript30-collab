79b5a771ce0143d942f027e89199d961d0c70bc5
AdrianSkar <adrian@adrianskar.com>
2023-02-03 13:47:12 +0100

    update[adrian-19]: video srcObject working

--- ProjectFiles-Adrian/19 - Webcam fun/scripts.js
+++ ProjectFiles-Adrian/19 - Webcam fun/scripts.js
@@ -3,3 +3,20 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+	navigator.mediaDevices
+		.getUserMedia({ video: true, audio: false })
+		.then(localMediaStream => {
+			console.log(localMediaStream);
+			// Deprecated
+			// video.src = window.URL.createObjectURL(localMediaStream);
+			video.srcObject = localMediaStream;
+			video.play();
+		})
+		.catch(err => {
+			console.error(`Error: ${err}`);
+		});
+}
+
+getVideo();
