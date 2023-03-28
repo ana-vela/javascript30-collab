dfb687cce920c7396b768585bc34d9081c741f52
AdrianSkar <adrian@adrianskar.com>
2023-03-28 17:30:14 +0200

    update[adrian-23]: complete exercise
    Finish exercise including lang filters, `msg` options and button
    functionality.

--- ProjectFiles-Adrian/23 - Speech synthesis/script.js
+++ ProjectFiles-Adrian/23 - Speech synthesis/script.js
@@ -12,6 +12,7 @@ msg.text = document.querySelector('[name="text"]').value;
 function populateVoices() {
 	// voices = this.getVoices();
 	voicesDropdown.innerHTML = voices
+		.filter(voice => voice.lang.includes('en') || voice.lang.includes('es'))
 		.map(
 			voice =>
 				`<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
@@ -22,7 +23,29 @@ function populateVoices() {
 // Set voice
 function setVoice() {
 	msg.voice = voices.find(voice => voice.name === this.value);
+	toggle();
 }
+
+// Restart
+function toggle(startOver = true) {
+	speechSynthesis.cancel();
+	if (startOver) {
+		speechSynthesis.speak(msg);
+	}
+}
+
+// Update msg options
+function setOption() {
+	// eg: msg.pitch = 0.8, or msg.text = input;
+	msg[this.name] = this.value;
+	toggle();
+}
+
+// Speak
+speakButton.addEventListener('click', toggle);
+// Stop
+// stopButton.addEventListener('click', toggle.bind(null, false));
+stopButton.addEventListener('click', () => toggle(false));
 
 // This won't trigger on page load in Firefox but is needed for Chrome
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
@@ -33,3 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 voicesDropdown.addEventListener('change', setVoice);
+options.forEach(option => option.addEventListener('change', setOption));
