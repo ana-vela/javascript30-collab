4c76eaf2a34e077195aafcda54e42a203d2e1b18
AdrianSkar <adrian@adrianskar.com>
2023-05-07 19:27:11 +0200

    extra[adrian-29]: use toLocaleString instead of ternary
    Use it to display am/pm times + the actual `am/pm` value using the
    `toLocaleString` method of Date instead of relying on ternary operators.

--- ProjectFiles-Adrian/29 - Countdown timer/scripts.js
+++ ProjectFiles-Adrian/29 - Countdown timer/scripts.js
@@ -28,28 +28,37 @@ function displayTimeLeft(secs) {
 	const minutes = Math.floor(secs / 60);
 	const remainderSecs = secs % 60;
 	// const display = `${minutes}:${remainderSecs < 10 ? '0' : ''}${remainderSecs}`;
-	const	display = `${minutes}:${String(remainderSecs).padStart(2,'0')}`;
+	const display = `${minutes}:${String(remainderSecs).padStart(2, '0')}`;
 	timerDisplay.textContent = display;
 	document.title = `Timer: ${display}`;
 }
 
-function displayEndTime(timestamp){
+function displayEndTime(timestamp) {
 	const end = new Date(timestamp);
 	const hour = end.getHours();
 	const minutes = end.getMinutes();
-	const americanHour = hour > 12 ? hour - 12 : hour;
+	// const americanHour = hour > 12 ? hour - 12 : hour;
+	//
+	const americanHour = end.toLocaleString('en-US', {
+		hour: 'numeric',
+		minute: 'numeric',
+		hour12: true,
+	});
 	// endTimeDisplay.textContent = `Be back at: ${hour}:${minutes < 10 ? '0' : ''}${minutes} (${americanHour}:${minutes < 10 ? '0' : ''}${minutes})`;
-	endTimeDisplay.textContent = `Be back at: ${hour}:${String(minutes).padStart(2, '0')} (${americanHour}:${String(minutes).padStart(2, '0')})`;
+	// endTimeDisplay.textContent = `Be back at: ${hour}:${String(minutes).padStart(2, '0')} (${americanHour}:${String(minutes).padStart(2, '0')})`;
 
+	const formattedHour = String(hour).padStart(2, '0');
+	const formattedMinutes = String(minutes).padStart(2, '0');
+	endTimeDisplay.textContent = `Be back at: ${formattedHour}:${formattedMinutes} (${americanHour})`;
 }
 
-function startTimer(){
+function startTimer() {
 	const secs = this.dataset.time;
 	timer(secs);
 }
 
 timerButtons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e){
+document.customForm.addEventListener('submit', function (e) {
 	e.preventDefault();
 	const mins = this.minutes.value;
 	this.reset();
