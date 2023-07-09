4133096f9b8c22575d2502a0e2cdeff8b9dbcac2
AdrianSkar <adrian@adrianskar.com>
2023-07-09 18:57:57 +0200

    feat[showcase]: load images following filenames

--- showcase/script.js
+++ showcase/script.js
@@ -5,27 +5,25 @@ import { paths } from './exercise-paths.js';
 const container = document.querySelector('.exercise-container');
 let frag = document.createDocumentFragment();
 
-function createCard(exPath, author) {
+function createCard(exPath, authorPath, authorName) {
 	const card = document.createElement('div');
 	card.classList.add('exercise-card');
 
 	// Title
 	const title = document.createElement('h3');
-	title.textContent = exPath.replace(
-		/^(\.\.\/ProjectFiles-(Adrian|Ana)\/)/,
-		''
-	);
+	title.textContent = exPath;
 	card.appendChild(title);
 
 	// Image
 	const image = document.createElement('img');
-	image.src = './screenshot-sample.avif'; 
+	const imagePath = `./images/${authorName}/${authorName}-${exPath}.avif`;
+	image.src = imagePath ?? './screenshot-sample.avif';
 	card.appendChild(image);
 
 	// Link
 	const link = document.createElement('a');
 	link.textContent = 'Demo';
-	link.href = author; 
+	link.href = authorPath;
 	card.appendChild(link);
 
 	return card;
@@ -34,8 +32,8 @@ function createCard(exPath, author) {
 // Iterate over exercises and create cards for each of them
 for (const exercise in paths) {
 	const { ana, adrian } = paths[exercise];
-	const cardAna = createCard(exercise, ana);
-	const cardAdrian = createCard(exercise, adrian); 
+	const cardAna = createCard(exercise, ana, 'Ana');
+	const cardAdrian = createCard(exercise, adrian, 'Adrian');
 	frag.append(cardAna, cardAdrian);
 }
 
