f8cbf4a85d90f7af9d3752012899456051597d63
AdrianSkar <adrian@adrianskar.com>
2023-09-30 20:45:52 +0200

    style[showcase]: display exercises on the same card

--- showcase/script.js
+++ showcase/script.js
@@ -4,7 +4,7 @@ import { paths } from './showcase-data.js';
 const container = document.querySelector('.exercise-container');
 let frag = document.createDocumentFragment();
 
-function createCard({ exName, exPath, authorPath, authorName }) {
+function createCard({ exName, exPath, authors }) {
 	const card = document.createElement('div');
 	card.classList.add('exercise-card');
 
@@ -13,28 +13,35 @@ function createCard({ exName, exPath, authorPath, authorName }) {
 	project.textContent = exName;
 	card.appendChild(project);
 
-	// Image
-	const image = document.createElement('img');
-	const imagePath = `./images/${authorName}/${authorName}-${exPath}.avif`;
-	image.src = imagePath ?? './screenshot-sample.avif';
-	card.appendChild(image);
+	authors.forEach(author => {
+		// Image
+		const image = document.createElement('img');
+		const imagePath = `./images/${author.authorName}/${author.authorName}-${exPath}.avif`;
+		image.src = imagePath ?? './screenshot-sample.avif';
+		card.appendChild(image);
 
-	// Link
-	const link = document.createElement('a');
-	link.textContent = 'Demo';
-	link.href = authorPath;
-	card.appendChild(link);
+		// Link
+		const link = document.createElement('a');
+		link.textContent = 'Demo';
+		link.href = author.authorPath;
+		card.appendChild(link);
+	});
 
 	return card;
 }
 
+
 // Iterate over exercises and create cards for each of them
 for (const exercise in paths) {
 	const { ana, adrian, project: exName } = paths[exercise];
-	const cardAna = createCard({ exName, exPath: exercise, authorPath: ana, authorName: 'ana' });
-	const cardAdrian = createCard({ exName, exPath: exercise, authorPath: adrian, authorName: 'adrian' });
-	frag.append(cardAna, cardAdrian);
+	const authors = [
+		{ authorPath: ana, authorName: 'ana' },
+		{ authorPath: adrian, authorName: 'adrian' },
+	];
+	const card = createCard({ exName, exPath: exercise, authors });
+	frag.append(card);
 }
+
 
 
 container.appendChild(frag);
